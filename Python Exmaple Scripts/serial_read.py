3b022370c7bfd36feb0e565b02fa09dfc4f3620e
sherdilkhan <sherdil.niazee@gmail.com>
2022-11-05 17:30:05 +0500

    data logger file added CSV

---
+++ Python Exmaple Scripts/serial_read.py
@@ -0,0 +1,36 @@
+# for serial read and sleep functions
+import serial
+import time
+
+#for data logging
+import csv
+from datetime import datetime
+ 
+
+#initialize Serial Port 
+ser = serial.Serial('COM7', 9600, timeout=1)
+
+time.sleep(2)
+ 
+i = 0
+    
+
+
+while(True):
+    # Reading all bytes available bytes till EOL
+    line = ser.readline() 
+    if line:
+        # Converting Byte Strings into unicode strings
+        string = line.decode()
+        dataSplit = string.split(',')  
+        # Converting Unicode String into integer
+        val1 = dataSplit[0]
+        val2 = dataSplit[1]
+
+        #num = int(string) 
+        print(val1 + ' , ' + val2)
+    with open("test.csv", "a") as nice:
+        nice.write(f"{i}, {val1}, {val2}")
+        i += 1
+ 
+ser.close()
---
+++ Python Exmaple Scripts/serial_test.py
@@ -0,0 +1,20 @@
+import serial
+
+ser = serial.Serial(
+    port='COM7',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+print("connected to: " + ser.portstr)
+count=1
+
+while True:
+    for line in ser.read():
+
+        print(str(count) + str(': ') + chr(line) )
+        count = count+1
+
+ser.close()
