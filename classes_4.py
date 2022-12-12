21ba80fbd9481bb480e0c34cea1b829b8f35c5b5
sherdilkhan <sherdil.niazee@gmail.com>
2022-12-12 22:34:27 +0500

    Update classes_4.py

--- classes_4.py
+++ classes_4.py
@@ -10,3 +10,12 @@ class Dog:
 pup1 = Dog("puppy", 12) #lets instantiate an object from class Dog. Now this objects has all the attributes of class Dog
 
 
+print (pup1.name)
+print (pup1.age)
+print (pup1.specie)
+
+pup1.specie = 'husky'  #class attributes can be changed dynamically
+pup1.age = 8           #instance attributes can also be chnaged dynamically
+
+print (pup1.specie)
+print (pup1.age)
