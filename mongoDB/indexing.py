3178af521f2a1592f08874dfa63051ff108c4849
sherdilkhan <sherdil.niazee@gmail.com>
2023-02-03 22:56:29 +0500

    1

--- mongoDB/indexing.py
+++ mongoDB/indexing.py
@@ -1,20 +1,28 @@
 
+import time
 from pymongo import *
-from os 
-
+import os
+import subprocess as sp
 
 mongopass = os.getenv("MONGOPASS") #get connection string from user enviroment variable
 client = MongoClient(mongopass) #create mongo client instance to connect with MongoDB online Cluster
-db = client.db_rm96_001 #create a database
-collection_1 = db.bd #create a document collection
+db_rm96_001 = client.db_rm96_001 #create a database
+collection_1 = db_rm96_001.collection_1 #create a document collection
 
 while True:
     date_stamp = sp.getoutput("date /t")
     time_stamp = sp.getoutput('time /t')
-    v_l1n = instrument.read_float(registeraddress=19000, functioncode=3,number_of_registers=2,byteorder = 0)  # Registernumber, number of decimals
-    f = instrument.read_float(registeraddress=19050, functioncode=3,number_of_registers=2,byteorder = 0)  # Registernumber, number of decimals 
     try:
-
+        myVal = {
+        'date': date_stamp,
+        'time': time_stamp,
+        'v_l1n': '240',
+        'frequency': '50' 
+        }
         x = collection_1.insert_one(myVal)
     except:
         pass
+    time.sleep(2)
+    
+
+    
